f569cd3d0c26efe273f64fd406a426d3e91e8964
ErdenArt <76409165+ErdenArt@users.noreply.github.com>
2025-09-20 13:31:53 +0200

    Update. I did a lot but also I dont know what

--- Engine/GameObject.cs
+++ Engine/GameObject.cs
@@ -1,29 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EngineArt.Engine
 {
-    public abstract class GameObject
+    internal class GameObject
     {
-        Vector2 position;
-        public float X { get => position.X; set => position.X = value; }
-        public float Y { get => position.Y; set => position.Y = value; }
-        public Vector2 Position { get => new Vector2(position.X, -position.Y); set => position = value; }
-        GameObject parent;
-        public GameObject Parent { get => parent; set => parent = value; }
-        public List<GameObject> Children { get; }
-
-        public GameObject()
-        {
-            Children = new List<GameObject>();
-        }
-        protected abstract void UpdateForParent();
-        public void AddChild(GameObject gameObject)
-        {
-            Children.Add(gameObject);
-            gameObject.Parent = this;
-
-            gameObject.UpdateForParent();
-        }
     }
 }
