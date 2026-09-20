f342ed6bd469b530a26680dd6c0f64eba0b6b6b4
ErdenArt <76409165+ErdenArt@users.noreply.github.com>
2026-09-20 19:05:54 +0200

    Preparing 3D model class

---
+++ src/3D/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace EngineArt.src._3D
+{
+    internal class Model
+    {
+        public Model(string fileName, Vector3 position)
+        {
+
+        }
+    }
+}
